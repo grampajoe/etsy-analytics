e08064c487a87322bd074d86c91ac71357643801
Joe Friedl <joe@joefriedl.net>
2015-06-21 07:31:47 -0400

    Echo things and stuff

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -ev
+
 npm install -g browserify
 browserify index.js > static/analytics.js
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ev
+
 git checkout gh-pages
 git add -f static
 git commit -m "Deploy"
